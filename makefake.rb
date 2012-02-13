5bcbdc6f7031cba7c0f7fe7be9680efa7416dc84
Luke Ehresman <luke@ehresman.org>
2012-02-13 09:24:01 -0500

    added random sha1 hash to simulate a password field

--- makefake.rb
+++ makefake.rb
@@ -13,13 +13,14 @@ opts = OptionParser.new do |opts|
 
   opts.on("--records n", Integer, "Number of records to generate", '=MANDATORY') { |n| options[:count] = n }
 
-  opts.on("--fields x,y,z", Array, "Random fields to include", "Fields can be: city, zip, surname, firstname") do |fields|
+  opts.on("--fields x,y,z", Array, "Random fields to include", "Fields can be: city, zip, surname, firstname, sha1hash") do |fields|
     fields.each do |field|
       case field
         when 'city' then options[:fields].push(RandomUSCity.new US_GEOGRAPHY)
         when 'zip' then options[:fields].push(RandomUSZip.new US_GEOGRAPHY)
         when 'surname' then options[:fields].push(RandomUSSurname.new US_SURNAMES)
         when 'firstname' then options[:fields].push(RandomUSFirstname.new US_FIRSTNAMES)
+        when 'sha1hash' then options[:fields].push(RandomSha1Hash.new)
         else
           puts "Invalid field #{field}"
           puts opts
