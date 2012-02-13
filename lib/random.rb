42c7260c4e06ff34ea04ab01ddc929ec16d7bbd7
Luke Ehresman <luke@ehresman.org>
2012-02-13 09:02:24 -0500

    added first names

--- lib/random.rb
+++ lib/random.rb
@@ -1,3 +1,4 @@
+require File.join(File.dirname(__FILE__), 'random_us_firstname.rb')
 require File.join(File.dirname(__FILE__), 'random_us_surname.rb')
 require File.join(File.dirname(__FILE__), 'random_us_zip.rb')
 require File.join(File.dirname(__FILE__), 'random_us_city.rb')
--- makefake.rb
+++ makefake.rb
@@ -5,6 +5,7 @@ require File.join(File.dirname(__FILE__), 'lib', 'random.rb')
 
 US_GEOGRAPHY=File.join(File.dirname(__FILE__), 'data', 'us_geography.csv')
 US_SURNAMES=File.join(File.dirname(__FILE__), 'data', 'surnames.csv')
+US_FIRSTNAMES=File.join(File.dirname(__FILE__), 'data', 'firstnames.csv')
 
 options={:fields => []}
 opts = OptionParser.new do |opts|
@@ -12,12 +13,13 @@ opts = OptionParser.new do |opts|
 
   opts.on("--records n", Integer, "Number of records to generate", '=MANDATORY') { |n| options[:count] = n }
 
-  opts.on("--fields x,y,z", Array, "Random fields to include", "Fields can be: city, zip, surname") do |fields|
+  opts.on("--fields x,y,z", Array, "Random fields to include", "Fields can be: city, zip, surname, firstname") do |fields|
     fields.each do |field|
       case field
         when 'city' then options[:fields].push(RandomUSCity.new US_GEOGRAPHY)
         when 'zip' then options[:fields].push(RandomUSZip.new US_GEOGRAPHY)
         when 'surname' then options[:fields].push(RandomUSSurname.new US_SURNAMES)
+        when 'firstname' then options[:fields].push(RandomUSFirstname.new US_FIRSTNAMES)
         else
           puts "Invalid field #{field}"
           puts opts
